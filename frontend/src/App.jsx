af7ffe1e1b6d1892745714f0f7f069840fd43c44
Bipulgrg <bipulgrg789@gmail.com>
2025-01-27 19:58:17 +0545

    fixed issues in login signup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom'; 
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Nav from './components/nav/nav'
@@ -9,22 +9,31 @@ import SignUp from './pages/signup/signup'
 import Hero from './components/heroSection/heroSection';
 import HomePage from './pages/homepage/homepage';
 function App() {
-  const [count, setCount] = useState(0)
+  const location = useLocation();
 
   return (
     <>
-    <Router>
       <Nav />
       <Routes>
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/" element={
+          <>
+            <Hero />
+            <HomePage />
+          </>
+        } />
       </Routes>
-    </Router>
-    <Hero />
-    <HomePage />
-    <Footer />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default function AppWrapper() {
+  return (
+    <Router>
+      <App />
+    </Router>
+  );
+}
+
